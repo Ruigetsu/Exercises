a39f8e0703b367d401e6561d0b2ce71bf0625a7c
Anton <dynovskiyanton@gmail.com>
2024-02-06 20:13:21 +0300

    Ex7

---
+++ Factorial Trailing Zeroes/solution.py
@@ -0,0 +1,11 @@
+def trailing_zeroes(N): 
+  counter = 1
+  factorial = 1
+  x = 0
+  for i in range(1,N+1):
+      factorial *= i 
+      i += 1 
+  while str(factorial)[-1] == "0":
+      factorial //= 10
+      x += 1
+  return x 
---
+++ Factorial Trailing Zeroes/tests.md
@@ -0,0 +1,15 @@
+test 1:
+Input: n = 0
+Output: 0
+
+test 2: 
+Input: n = 5
+Output: 1
+
+test 3:
+Input: n = 24
+Output: 4
+
+test 4:
+Input: n = 100
+Output: 24

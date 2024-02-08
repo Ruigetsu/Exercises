0e297b257e190e830539c9272ef84515c2e9f883
Anton <dynovskiyanton@gmail.com>
2024-02-08 20:22:34 +0300

    json

---
+++ json/json.py
@@ -0,0 +1,11 @@
+import json
+users = {
+    "name": "Ivan", 
+    "surname": "Drago",
+    "email": "IvanDrago@mail.ru"
+}
+
+json_data = json.dumps(users)
+print(json_data)
+data = json.loads(json_data)
+print(data['email'])

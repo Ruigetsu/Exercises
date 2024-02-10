3382be47c1e00471fdfc682702cf72d41d19c9dd
Anton <dynovskiyanton@gmail.com>
2024-02-10 17:57:16 +0300

    classes js

---
+++ classes(js)/example.js
@@ -0,0 +1,15 @@
+let car = new Object(),
+    car1 = {
+        color: "Black",
+        mark: "BMW",
+        model: "M8",
+        cost: "100 000 $"
+    }   
+    car2 = {
+        color: "Red",
+        mark:"Audi",
+        model:"R8",
+        cost:"75 000 $"
+    }
+console.log(`My first car - ${car1.color} ${car1.mark} ${car1.model}, it's cost around ${car1.cost}`)
+console.log(`My second car - ${car2.color} ${car2.mark} ${car2.model}, it's cost around ${car2.cost}`)

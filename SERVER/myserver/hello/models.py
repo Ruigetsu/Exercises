54d7127d028619ba203a73e16e28f552022b19f2
Anton <dynovskiyanton@gmail.com>
2024-02-20 13:30:50 +0300

    new one

--- SERVER/myserver/hello/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Post(models.Model):
-    id = models.IntegerField()
-    title = models.CharField()
-    body = models.CharField()
-
-    class Meta:
-        ordering = ['created']
--- SERVER/myserver/hello/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from rest_framework.renderers import JSONRenderer
-
-#class PostSerializer(serializers.Serializer):
-    #id  = serializers.IntegerField()
-    #title = serializers.CharField()
-    #body = serializers.CharField()
